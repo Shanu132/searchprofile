1014a9a1b3ba26bc4d02fecdf4ec09b8d2eb7178
Shanu Karan <Shanu.karan.07@gmail.com>
2022-06-23 21:35:04 +0530

    Updates

---
+++ precache-manifest.f37dbc02ced95f80a5e8d8a304070838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "01043686923729d0e25f",
+    "url": "/searchprofile/static/css/main.1bc39924.chunk.css"
+  },
+  {
+    "revision": "01043686923729d0e25f",
+    "url": "/searchprofile/static/js/main.01043686.chunk.js"
+  },
+  {
+    "revision": "66c154a22e2b7b2b9b37",
+    "url": "/searchprofile/static/js/1.66c154a2.chunk.js"
+  },
+  {
+    "revision": "09d59c68df725fa4b32f",
+    "url": "/searchprofile/static/css/2.78723217.chunk.css"
+  },
+  {
+    "revision": "09d59c68df725fa4b32f",
+    "url": "/searchprofile/static/js/2.09d59c68.chunk.js"
+  },
+  {
+    "revision": "0b71370f66d3bd8ebe25",
+    "url": "/searchprofile/static/js/runtime~main.0b71370f.js"
+  },
+  {
+    "revision": "19cde3ce4270ceaf58d9c73414ab46ff",
+    "url": "/searchprofile/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/searchprofile/precache-manifest.f37dbc02ced95f80a5e8d8a304070838.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/searchprofile/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
